226e3611df7d8b8adf1539be4f8718d0fedf8baa
Francesco Paciello <kekkopacio@gmail.com>
2021-01-26 14:15:01 +0100

    Initial Commit

---
+++ mcdonald/mcdonaldApp.swift
@@ -0,0 +1,17 @@
+//
+//  mcdonaldApp.swift
+//  mcdonald
+//
+//  Created by Kekko Paciello on 26/01/21.
+//
+
+import SwiftUI
+
+@main
+struct mcdonaldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
